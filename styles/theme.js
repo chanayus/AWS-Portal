2ad12b931606aa5b4e987b41b1b44c6b98f37dd9
Xhanayus <chanayus1526@hotmail.com>
2021-09-30 16:28:01 +0700

    update Documents

--- styles/theme.js
+++ styles/theme.js
@@ -1,10 +1,19 @@
 
 export const theme = {
-    black : "#050505",
-    blue: "#2E75DF",
-    white: "#FFF",
-    bgColor: "#E6E9EE",
-    red: "#E82424",
-    tabletScreen: "(max-width: 860px)"
+    light: {
+        textColor : "#050505",
+        mainColor: "#E6E9EE",
+        subColor: "#FFF",
+        blue: "#2E75DF",
+        red: "#E82424",
+        tabletScreen: "(max-width: 960px)"
+    },
+    dark: {
+        textColor : "#FFF",
+        mainColor: "#242424",
+        subColor: "#050505",
+        blue: "#2E75DF",
+        red: "#E82424",
+        tabletScreen: "(max-width: 960px)"
+    },
 }
-
