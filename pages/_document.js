398fa4b2939375e7e6ad01292f51431caa6e49aa
t <38640424+poohcid@users.noreply.github.com>
2021-10-07 23:20:23 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap" rel="stylesheet" crossorigin="anonymous"/>
         </Head>
         <body>
           <Main />
