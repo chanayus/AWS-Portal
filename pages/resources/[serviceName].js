c5bdcc70e917338e54c3a0c178917ddd60169cf6
Xhanayus <chanayus1526@hotmail.com>
2021-12-06 15:36:20 +0700

    add category in resource detail page

--- pages/resources/[serviceName].js
+++ pages/resources/[serviceName].js
@@ -5,18 +5,32 @@ import Breadcrumb from "../../components/main/Breadcrumb";
 import Image from "next/image";
 import Loading from "../../components/main/loading";
 import ResourceTable from "../../components/resource/ResourceTable";
+import { getUniqueResourceType } from "../../lib/getUniqueData";
+import styled from "styled-components";
 import { useFetch } from "../../lib/useFetch";
 import { useRouter } from "next/router";
 
 const SpecificResource = () => {
   const router = useRouter();
   const [resources, setResources] = useState([]);
-  const { serviceName } = router.query;
+  const { serviceName, resource_type } = router.query;
   const { loading, data } = useFetch("/api/resources", setResources, true);
+  const [currentType, setCurrentType] = useState(resource_type ? resource_type : "all");
+  const [resourceType, setResourceType] = useState([]);
 
   useEffect(() => {
     setResources(data.filter((value) => value.serviceName === serviceName));
+    setResourceType(getUniqueResourceType(data, serviceName));
   }, [data]);
+
+  const changeType = (typeValue) => {
+    if (typeValue === "all") {
+      setResources(data.filter((value) => value.serviceName === serviceName));
+    } else {
+      setResources(data.filter((value) => value.resourceType === typeValue));
+    }
+    setCurrentType(typeValue);
+  };
 
   if (loading) {
     return (
@@ -28,11 +42,27 @@ const SpecificResource = () => {
   } else {
     return (
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.4 }}>
-        <Breadcrumb/>
-        <div className="flex items-center mb-16">
+        <Breadcrumb />
+        <div className="flex items-center mb-8">
           <Image className="rounded" src={`/images/resourceIcon/${serviceName}.png`} alt="service-Img" width={50} height={50} />
           <h1 className="ml-3 capitalize">{serviceName}</h1>
         </div>
+        <Grid className="lg:grid-cols-2 md:gap-y-2 gap-3">
+          {resourceType.length === 1 ? null : (
+            <>
+              <button className={`flex justify-between items-center p-4 py-3 dynamic-bg shadow rounded-sm ${currentType === "all" ? "active" : null}`} onClick={() => changeType("all")}>
+                <p>ทั้งหมด</p>
+                <h2 className="text-xl">{data.filter((value) => value.serviceName === serviceName).length}</h2>
+              </button>
+              {resourceType.map((value, index) => (
+                <button key={index} className={`flex justify-between items-center p-4 py-3 dynamic-bg shadow rounded-sm ${currentType === value ? "active" : null}`} onClick={() => changeType(value)}>
+                  <p className="capitalize text-left">{value}</p>
+                  <h2 className="text-xl">{data.filter((value) => value.serviceName === serviceName).filter((item) => item.resourceType === value).length}</h2>
+                </button>
+              ))}
+            </>
+          )}
+        </Grid>
         <AnimatePresence exitBeforeEnter>
           <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.25 }} key={"table"}>
             <ResourceTable resources={resources} setResources={setResources} />
@@ -42,5 +72,17 @@ const SpecificResource = () => {
     );
   }
 };
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  @media (max-width: 480px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  .active {
+    transition: 0.25s;
+    box-shadow: 0 0 3px 2px ${(props) => props.theme.blue};
+  }
+`;
 
 export default SpecificResource;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       '2xs': {'raw': '(max-width: 360px)'},
       xs: {'raw': '(max-width: 480px)'},
       sm: {'raw': '(max-width: 640px)'},
-      md: {'max': '960px'},
+      md: {'raw': '(max-width: 960px)'},
       lg: {'raw': '(max-width: 1024px)'}, 
       xl: {'raw': '(max-width: 1280px)'},
       "2xl" : {'raw': '(max-width: 1366px)'},
