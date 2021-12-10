bdbba51cdc501d3e115ceb39ba289cc89769e008
Xhanayus <chanayus1526@hotmail.com>
2021-12-10 14:00:39 +0700

    Update resources.js

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
   const fetch = require('node-fetch');
-  const request = await fetch(`https://wxqwiwu0zd.execute-api.ap-southeast-1.amazonaws.com/get-resource/?fbclid=IwAR08iEw3fWT7rxPZpZT5CIXRA-MXVoFAZ9jVmG-FQvjXm3XqLvkZYvdMhJk`)
+  const request = await fetch(`https://wxqwiwu0zd.execute-api.ap-southeast-1.amazonaws.com/get-resource/`)
   const dataJson = await request.json()
   res.json(dataJson)
 }
