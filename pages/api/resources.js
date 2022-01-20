88198022564fb9f5608a8884692258de0be3febe
t <38640424+poohcid@users.noreply.github.com>
2022-01-20 16:49:19 +0700

    use env. get resources end point

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,8 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default async function handler(req, res) {
   const fetch = require('node-fetch');
-  const request = await fetch(`https://wxqwiwu0zd.execute-api.ap-southeast-1.amazonaws.com/get-resource/`)
+  const request = await fetch(`${process.env.GET_RESOURCES_END_POINT}/get-resources/`)
   const dataJson = await request.json()
   res.json(dataJson)
 }
